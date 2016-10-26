5d9c9227abf2cb5b4516a3d477cc1cb9bef75be5
wunnle <me@wunnle.com>
2016-10-26 04:04:57 +0300

    scss and animations

--- js/common.js
+++ js/common.js
@@ -1,12 +1,22 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    $("body").addClass("r");
     headerSwipe();
+
+    for (i = 0; i < $("article").length; i++) {
+        setDelay(i, 'r', 215);
+    }
+
 });
 
 function headerSwipe() {
-
-    setTimeout(function () {
+    setTimeout(function() {
         console.log('ITS TIME!');
         $('header a').addClass('goLeft');
     }, 2000);
+}
 
+function setDelay(i, className, interval) {
+    setTimeout(function() {
+        $("body").find('article').eq(i).addClass(className);
+    }, i * interval)
 }
